d93b705a8a3bf920beb0d3df4b59041e11ce44a0
Eswar <53896142+ioeshu@users.noreply.github.com>
2020-12-29 13:29:43 +0530

    Add files via upload

---
+++ ig_bot.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+class InstaBot:
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+        self.bot = webdriver.Firefox()
+    
+    def Login(self):
+        bot = self.bot
+        bot.get("https://www.instagram.com/accounts/login/")
+        time.sleep(2)
+        username = bot.find_element_by_name('username')
+        password = bot.find_element_by_name('password')
+        username.clear()
+        password.clear()
+        username.send_keys(self.username)
+        password.send_keys(self.password)
+        time.sleep(2)
+        bot.find_element_by_class_name('Igw0E.IwRSH.eGOV_._4EzTm.bkEs3.CovQj.jKUp7.DhRcB').click()
+        time.sleep(5)
+
+
+    def openStories(self):
+        bot = self.bot
+        bot.find_element_by_class_name('sqdOP.yWX7d.y3zKF').click()
+        time.sleep(2)
+        bot.find_element_by_class_name('aOOlW.HoLwm ').click()
+        # stroies : c6Ldk
+        time.sleep(2)
+        bot.find_element_by_class_name('_6q-tv').click()
+
+    def next(self):
+        bot = self.bot
+        bot.find_element_by_class_name('coreSpriteRightChevron').click()
+        time.sleep(1)
+        
+go = InstaBot('e.s.war','#Eswar0103.A')
+go.Login()
+go.openStories()
+go.next()
